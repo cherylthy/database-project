b19980c94695d53703ea7a63e4506a9f7d33806d
cherylthy <59245154+cherylthy@users.noreply.github.com>
2023-11-06 17:22:04 +0800

    updated codes for mysql and firebase

--- main.py
+++ main.py
@@ -1,33 +1,52 @@
-from flask import Flask, render_template
-import firebase_admin
-from firebase_admin import credentials, db, firestore
+from flask import Flask, render_template, jsonify
 from firebase import Firebase
+from flaskext.mysql import MySQL
 import uuid
 
-# Initialize Flask
 app = Flask(__name__)
 
 # Initialize Firebase
 crud = Firebase()
 
+# Initialize MySQL
+mysql = MySQL()
+
+# MySQL configuration
+app.config['MYSQL_DATABASE_HOST'] = '35.212.183.7'
+app.config['MYSQL_DATABASE_USER'] = 'chery'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'dbpassword'
+app.config['MYSQL_DATABASE_DB'] = 'inf2003'
+
+mysql.init_app(app)
+
 # Generate random document id
 def generate_document_id():
-        # Generate a random unique identifier
-        unique_id = str(uuid.uuid4())
-        return f"reviews-{unique_id}"
+    # Generate a random unique identifier
+    unique_id = str(uuid.uuid4())
+    return f"reviews-{unique_id}"
 
 @app.route('/')
 def display_reviews():
     result = crud.get("reviews")
-
     document_id = generate_document_id()
-
+    
     # Add a document to a collection with a specific ID
-    data = {"userID": 5432, "carID": 3333}
-    crud.add('reviews', data=data, document_id= document_id)
-
-
+    data = {"userID": 5432, "carID": 3333, "comments": "This car sucks", "ratings":4.5}
+    crud.add('reviews', data=data, document_id=document_id)
+    
     return render_template('test.html', reviews=result)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/select_all')
+def select_all_from_table():
+    try:
+        cursor = mysql.get_db().cursor()
+        cursor.execute("SELECT * FROM CarInventory")
+        data = cursor.fetchall()
+        cursor.close()
+        return render_template('sql_test.html', data=data)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- python app.py
+++ python app.py
@@ -1,54 +1,29 @@
 from flask import Flask, jsonify
-from flask_mysqldb import MySQL
-import sshtunnel
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
-# SSH tunnel connection details
-ssh_host = '35.212.183.7'
-ssh_port = 22
-ssh_user = 'dev1'
-ssh_password = 'inf2003@DB'
+mysql = MySQL()
 
-# MySQL server details
-mysql_host = 'localhost'
-mysql_port = 3306  # Use the correct MySQL port here
-mysql_user = 'dev1'
-mysql_password = 'inf2003@DB'
-mysql_db = 'inf2003'
+# MySQL configuration
+app.config['MYSQL_DATABASE_HOST'] = '35.212.183.7'
+app.config['MYSQL_DATABASE_USER'] = 'chery'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'dbpassword'
+app.config['MYSQL_DATABASE_DB'] = 'inf2003'
 
-# Create the SSH tunnel
-with sshtunnel.SSHTunnelForwarder(
-    (ssh_host, ssh_port),
-    ssh_username=ssh_user,
-    ssh_password=ssh_password,
-    remote_bind_address=('127.0.0.1', mysql_port)
-) as tunnel:
-    if tunnel.is_active:
-        print("SSH tunnel successfully created.")
-        
-        # Configure MySQL connection to use the SSH tunnel
-        app.config['MYSQL_HOST'] = 'localhost'  # Use 'localhost' when using an SSH tunnel
-        app.config['MYSQL_PORT'] = 3306
-        app.config['MYSQL_USER'] = mysql_user
-        app.config['MYSQL_PASSWORD'] = mysql_password
-        app.config['MYSQL_DB'] = mysql_db
+mysql.init_app(app)
 
-        mysql = MySQL(app)
-    else:
-        print("SSH tunnel creation failed.")
-        raise Exception("SSH tunnel creation failed")
-
-    @app.route('/')
-    def select_all_from_table():
-        try:
-            cursor = mysql.connection.cursor()
-            cursor.execute("SELECT * FROM CarInventory")
-            data = cursor.fetchall()
-            cursor.close()
-            return jsonify(data)
-        except Exception as e:
-            return jsonify({'error': str(e)})
+@app.route('/')
+@app.route('/select_all')
+def select_all_from_table():
+    try:
+        cursor = mysql.get_db().cursor()
+        cursor.execute("SELECT * FROM CarInventory")
+        data = cursor.fetchall()
+        cursor.close()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
