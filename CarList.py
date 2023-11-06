aed7089512131e47c3789c8465ca35ba106196f8
Khan Rasheed <khanrasheed472@hotmail.com>
2023-11-07 01:16:52 +0800

    Created Static folder, CarList HTML and Python
    Created Static folder, CarList HTML and Python

---
+++ CarList.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, jsonify
+from firebase import Firebase
+from flaskext.mysql import MySQL
+import uuid
+
+app = Flask(__name__)
+
+# Initialize Firebase
+crud = Firebase()
+
+# Initialize MySQL
+mysql = MySQL()
+
+# MySQL configuration
+app.config['MYSQL_DATABASE_HOST'] = '35.212.183.7'
+app.config['MYSQL_DATABASE_USER'] = 'khan'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'dbpassword'
+app.config['MYSQL_DATABASE_DB'] = 'inf2003'
+
+mysql.init_app(app)
+
+
+@app.route('/CarList')
+def select_all_from_table():
+    try:
+        cursor = mysql.get_db().cursor()
+        cursor.execute("SELECT CarMake, CarModel FROM CarInventory")
+        data = cursor.fetchall()
+        cursor.close()
+        return render_template('cars.html', data=data)
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
