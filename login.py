7b65de2fe7ac325336cacf35d09c6bf7cdd1cf49
samuelgjy <samuelgjy97@gmail.com>
2023-11-07 15:16:35 +0800

    register done
    login, register done, able to save to db

--- login.py
+++ login.py
@@ -51,5 +51,55 @@ def login():
     # Show the login form with message (if any)
     return render_template('login.html', msg=msg)
 
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('UserID', None)
+   session.pop('Email', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'Name' in request.form and 'Password' in request.form and 'Email' in request.form:
+        # Create variables for easy access
+        name = request.form['Name']
+        password = request.form['Password']
+        email = request.form['Email']
+        # Check if account exists using MySQL
+        cursor = mysql.get_db().cursor()
+        cursor.execute('SELECT * FROM UserAccounts WHERE Email = %s', (email,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', name):
+            msg = 'Name must contain only characters!'
+        elif not name or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Hash the password
+            hash = password + app.secret_key
+            hash = hashlib.sha1(hash.encode())
+            password = hash.hexdigest()
+            # Account doesn't exist, and the form data is valid, so insert the new account into the UserAccounts table
+            cursor.execute('INSERT INTO UserAccounts VALUES (NULL, %s, %s, NULL, NULL, %s, NULL)', (password, name, email,))
+            mysql.get_db().commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
