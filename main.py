13e53d5d32c36dfd19a49a960596a00742b94375
cherylthy <59245154+cherylthy@users.noreply.github.com>
2023-11-07 17:34:53 +0800

    cleaned up codes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request, redirect, url_for, session
 from firebase import Firebase
 from flaskext.mysql import MySQL
 import uuid
+import MySQLdb.cursors
+import MySQLdb.cursors, re, hashlib
 
 app = Flask(__name__)
 
@@ -13,40 +15,96 @@ mysql = MySQL()
 
 # MySQL configuration
 app.config['MYSQL_DATABASE_HOST'] = '35.212.183.7'
-app.config['MYSQL_DATABASE_USER'] = 'chery'
+app.config['MYSQL_DATABASE_USER'] = 'common'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'dbpassword'
 app.config['MYSQL_DATABASE_DB'] = 'inf2003'
 
 mysql.init_app(app)
 
-# Generate random document id
-def generate_document_id():
-    # Generate a random unique identifier
-    unique_id = str(uuid.uuid4())
-    return f"reviews-{unique_id}"
+@app.route('/', methods=['GET', 'POST'])
+def login():
 
-# @app.route('/')
-# def display_reviews():
-#     result = crud.get("reviews")
-#     document_id = generate_document_id()
-    
-#     # Add a document to a collection with a specific ID
-#     data = {"userID": 5432, "carID": 3333, "comments": "This car sucks", "ratings":4.5}
-#     crud.add('reviews', data=data, document_id=document_id)
-    
-#     return render_template('test.html', reviews=result)
+    # Output a message if something goes wrong...
+    msg = ''
 
-@app.route('/')
-def select_all_from_table():
-    try:
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'Email' in request.form and 'Password' in request.form:
+
+        # Check if email and password exist in database
+        email = request.form['Email']
+        password = request.form['Password']
         cursor = mysql.get_db().cursor()
-        cursor.execute("SELECT * FROM CarInventory")
-        data = cursor.fetchall()
-        cursor.close()
-        return render_template('sql_test.html', data=data)
-    
-    except Exception as e:
-        return jsonify({'error': str(e)})
+        cursor.execute('SELECT * FROM UserAccounts WHERE Email = %s AND Password = %s', (email, password,))
+        account = cursor.fetchone()
+
+        # If account exists in accounts table in out database
+        if account:
+            session['loggedin'] = True
+            session['UserID'] = account[0]
+            session['Email'] = account[1]
+
+            return 'Logged in successfully!'
+        
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+
+    # Show the login form with message (if any)
+    return render_template('login.html', msg=msg)
+
+@app.route('/logout')
+def logout():
+   
+# Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('UserID', None)
+   session.pop('Email', None)
+
+   # Redirect to login page
+   return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    # Output message if something goes wrong...
+    msg = ''
+
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'Name' in request.form and 'Password' in request.form and 'Email' in request.form:
+
+        #Check for if account used to register exist in database
+        name = request.form['Name']
+        password = request.form['Password']
+        email = request.form['Email']
+        cursor = mysql.get_db().cursor()
+        cursor.execute('SELECT * FROM UserAccounts WHERE Email = %s', (email,))
+        account = cursor.fetchone()
+
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', name):
+            msg = 'Name must contain only characters!'
+        elif not name or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Hash the password
+            hash = password + app.secret_key
+            hash = hashlib.sha1(hash.encode())
+            password = hash.hexdigest()
+
+            # Account doesn't exist, and the form data is valid, so insert the new account into the UserAccounts table
+            cursor.execute('INSERT INTO UserAccounts VALUES (NULL, %s, %s, NULL, NULL, %s, NULL)', (password, name, email,))
+            mysql.get_db().commit()
+            msg = 'You have successfully registered!'
+
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+
+    return render_template('register.html', msg=msg)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
